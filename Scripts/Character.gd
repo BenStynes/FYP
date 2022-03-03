812944f709b781b9a7c62bfd32f90e6cf68409c8
Ben Stynes(Styner) <45253301+BenStynes@users.noreply.github.com>
2022-03-03 19:38:28 +0000

    Been doing isometric stuff....taking a while

--- Scripts/Character.gd
+++ Scripts/Character.gd
@@ -4,12 +4,18 @@ var tile_size = 64;
 var move_time = 0.25;
 var terrain = null;
 
+
+
+
+
+func cartesian_to_isometric(vector):
+	 return Vector2(vector.x - vector.y,(vector.x+vector.y)/2)
+
 func _ready():
 	terrain = get_tree().get_nodes_in_group("terrain")
 	terrain = terrain[0]
 
-func _process(delta):
-	
+func _process(_delta):
 	if not $Tween.is_active():
 		if Input.is_action_just_pressed("ui_left"):
 			_move(Vector2(-1,0));
@@ -22,9 +28,10 @@ func _process(delta):
 		
 func _move(direction):
 	var tile = Vector2(int(position.x/tile_size),int(position.y/tile_size))
-	if terrain.get_cellv(tile + direction) == -1:
+	var cart = cartesian_to_isometric(direction);
+	if terrain.get_cellv(tile + direction) != 0:
 		if direction.x != 0:
-			$Tween.interpolate_property(self,"position:x",position.x,position.x+direction.x*tile_size,move_time,Tween.TRANS_LINEAR,Tween.EASE_IN_OUT)
+			$Tween.interpolate_property(self,"position:x",position.x,position.x+cart.x*tile_size,move_time,Tween.TRANS_LINEAR,Tween.EASE_IN_OUT)
 		else:
-			$Tween.interpolate_property(self,"position:y",position.y,position.y+direction.y*tile_size,move_time,Tween.TRANS_LINEAR,Tween.EASE_IN_OUT)
+			$Tween.interpolate_property(self,"position:y",position.y,position.y+cart.y*tile_size,move_time,Tween.TRANS_LINEAR,Tween.EASE_IN_OUT)
 	$Tween.start();
